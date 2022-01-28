7f156c5fddb3b0f2bb55b4dd898f9712ecaaa850
3q6 <73487410+3q6@users.noreply.github.com>
2022-01-28 02:24:33 +0000

    Create fix.lua

---
+++ v3/fix.lua
@@ -0,0 +1,11 @@
+local fix = {}
+
+function fix:start(try)
+    print("Roblox's API Has restricted for any of this to work, please wait until the 1st of feb")
+end
+
+function try()
+    print("trying to call an arugment is false")
+end
+
+return fix
